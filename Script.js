fb7cf9b4a787b3118517c9ead5f01466cfff17f7
LastLight999 <sabazurabashvili123@gmail.com>
2022-07-16 00:13:56 +0400

    commited

--- Script.js
+++ Script.js
@@ -1,11 +1,12 @@
-let gridDimensions = 16;
-let minCellSize = 5;
+let gridDimensions = 64;
+let minCellSize = 2;
 let maxCellSize = 25;
 let mindimension = 0;
+let cellSize = 0;
 
 // waiting for Page to load to do everything
 window.onload = (event) =>{
-    console.log("test");
+    
     const Grid = document.querySelector("#Grid");
     CreateGrid(gridDimensions, Grid);
 
@@ -20,6 +21,42 @@ const CreateGrid = (gridSize, grid) => {
     }
 
     mindimension = Math.min(window.innerWidth,window.innerHeight);
-    console.log(mindimension);
+    
+    cellSize = CalculateCellSize(gridSize);
 
+    grid.setAttribute("Style",`
+        width:${(cellSize+2)*gridSize}px; 
+        height:${(cellSize+2)*gridSize}px;
+        background-color: red;
+        grid-template-columns:repeat(${gridSize},auto)`);
+    CreateCellGrid(gridSize, grid,cellSize);
+    
+}
+const CalculateCellSize = (gridSize) => {
+    let returnValue = maxCellSize;
+    
+    if(mindimension-50 <= (returnValue+2)* gridSize){
+        for(let i = returnValue; (i+2) * gridSize >= mindimension-50; i--){
+            
+            if(i <= minCellSize){
+                return minCellSize;
+            }
+            
+            returnValue = i;
+        }
+    }
+    return returnValue;
+}
+const CreateCellGrid = (gridSize,grid,cellSize) => {
+    for(let i = 0; i < gridSize; i++){
+        for(let j = 0; j < gridSize; j++){
+            console.log("jtest")
+            let tempchild = document.createElement("div");
+            grid.appendChild(tempchild);
+            tempchild.setAttribute("style",`border:solid 1px black; background-color:white; height:${cellSize}px; width:${cellSize }px; `)
+            tempchild.addEventListener("mouseover", (event) => {
+                tempchild.setAttribute("Style","background-color:green;")
+            })
+        }
+    }
 }
